33a3880e6e5cbe64c5ad4d11c7106f76725166a2
Brad Griffith <brad.s.griffith@gmail.com>
2021-04-29 11:55:16 -0400

    deploy to Heroku

--- app.js
+++ app.js
@@ -110,6 +110,11 @@ app.post("/", (req,res) => {
 });
 
 // Spin up the server 
-app.listen(3000, () => {
-    console.log("The server is running on port 3000.")
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+
+app.listen(port, () => {
+    console.log("Server has started successfully.")
 });
